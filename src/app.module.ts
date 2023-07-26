94a01fb025cf0409948c1ad110fc809d52a86368
Manasse Daniel <danidkm242@gmail.com>
2023-07-27 00:36:39 +0100

    enabling cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { TodoModule } from './todo/todo.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
